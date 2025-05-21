6754bdeca2c129c75e0a935f9c1058f270c4936e
lain456 <ahmedridha.lazhari@esprit.tn>
2025-05-21 23:34:39 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
-#include "init/init.h"
-#include "game/game.h"
-#include "load_menus/load_menus.h"
-#include "tools/tools.h"
-#include "Player/player.h"
-#include "Background/background.h"
-#include "button/button.h"
-#include "slider/slider.h"
-#include "audio/audio.h"
-#include "text/txt.h"
-#include "menu/menu.h"
-#include "arduino/arduino.h"
-#include "shity_code/shity_code.h"
-#include "my_input/my_input.h"
+#include <SDL/SDL.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_image.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
-    printf(" nope :P , contact lain456 on discord to get the original file ")
+    printf("game is still WIP...\n");
 }
+
+// contact lain456 on discord to get the original file :D
